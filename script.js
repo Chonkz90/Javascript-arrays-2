aa3cce78b6c36dcc22e0893200743141009e2a41
Chonkz90 <zahlerdal@yahoo.com>
2024-08-22 10:43:33 +0200

    buttons fruitbowl

---
+++ script.js
@@ -0,0 +1,23 @@
+/* Array Methods */
+
+const fruitBowl = ["Orange", "Banana", "Apple", "Kiwi"];
+console.log (fruitBowl);
+console.log (fruitBowl.toString());
+
+// .push legger til et element på slutten i en array
+fruitBowl.push("Mango");
+console.log(fruitBowl);
+
+// .unshift legger til et element på starten i en array
+fruitBowl.unshift("Pineapple");
+console.log (fruitBowl);
+
+// .pop fjerner elementet på slutten i en array
+fruitBowl.pop();
+fruitBowl.pop();
+console.log(fruitBowl);
+
+// .shift fjerner det første elementet i en array
+fruitBowl.shift();
+console.log(fruitBowl);
+
